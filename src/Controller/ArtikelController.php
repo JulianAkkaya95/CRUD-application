6650aa92600dc3e93ae66ca77f28b1dec3d251af
julian akkaya <julian.akkaya@icloud.com>
2020-01-12 13:12:08 +0100

    Add image upload service

--- src/Controller/ArtikelController.php
+++ src/Controller/ArtikelController.php
@@ -4,12 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Artikel;
 use App\Form\Type\ArtikelType;
+use App\Service\FileUploader;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\FormInterface;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -52,58 +50,43 @@ class ArtikelController extends AbstractController
      * Wenn kein Formular übergeben wurde, wird ein neuer Artikel erstellt, der über das Fomular gespeichert werden kann.
      * @Route("/edit/{id}", name="editMask")
      * @param Request $request
+     * @param FileUploader $fileUploader
      * @param int $id z.B. 1 oder 16
      * @return Response
      * @throws \Exception
      */
-    public function edit(Request $request, int $id = NULL): Response
+    public function edit(Request $request, FileUploader $fileUploader, int $id = NULL): Response
     {
         $entityManager = $this->getDoctrine()->getManager();
         if (isset($id)) {
             $artikel = $entityManager->getRepository(Artikel::class)->find($id);
         }
-        //Auch für im Fall das eine ungültige ID übergeben wurde, soll ein neuer Artikel erstellt werden.
+        //Im Fall das eine ungültige ID übergeben wurde, wird ein neuer Artikel erstellt.
         if (!isset($artikel)) {
             $artikel = new Artikel();
         }
 
         $form = $this->createForm(ArtikelType::class, $artikel);
         $form->handleRequest($request);
-        $this->saveEditArtikel($form, $artikel, $entityManager);
 
-        return $this->render('editMask.html.twig', [
-            "form" => $form->createView(),
-            "artikel" => $artikel
-        ]);
-    }
-
-    /**
-     * Speichert Artikel die durch das Formular verändert oder hinzugefügt worden sind.
-     * @param FormInterface $form
-     * @param Artikel $artikel
-     * @param ObjectManager $entityManager
-     * @throws \Exception
-     */
-    function saveEditArtikel(FormInterface $form, Artikel &$artikel, ObjectManager $entityManager)
-    {
         if ($form->isSubmitted() && $form->isValid()) {
             /** @var UploadedFile $image */
             $image = $form['image']->getData();
             if ($image) {
-                //Für jedes Bild muss ein eindeutiger Name erstellt werden.
-                $originalFilename = pathinfo($image->getClientOriginalName(), PATHINFO_FILENAME);
-                $newFilename = $originalFilename . '-' . uniqid() . '.' . $image->guessExtension();
-                $image->move(
-                    $this->getParameter('image_directory'),
-                    $newFilename
-                );
-                $artikel->setImage($newFilename);
+                $imageFileName = $fileUploader->upload($image);
+                $artikel->setImage($imageFileName);
             }
+
             $artikel = $form->getData();
             $artikel->setLastModified(new DateTime());
             $entityManager->persist($artikel);
             $entityManager->flush();
         }
+
+        return $this->render('editMask.html.twig', [
+            "form" => $form->createView(),
+            "artikel" => $artikel
+        ]);
     }
 
     /**
